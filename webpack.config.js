40396f4618fd239440b655ef85a121591daf5f8d
Madeleine <madeleine.auvinen@stinkstudios.com>
2017-11-30 14:12:13 -0500

    set up webpack config + babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack config
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+const output = path.resolve(__dirname + '/dist');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: output,
+    filename: 'js/index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
+}
