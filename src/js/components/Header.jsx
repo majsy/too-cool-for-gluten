567c59f734ba4a3c4f279768d6649baf05e72f88
Madeleine <madeleine.auvinen@stinkstudios.com>
2017-12-13 18:32:51 -0500

    remove header isShowing class

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,10 +4,10 @@ import styles from '../../scss/components/_header.scss'
 export default class Header extends React.Component {
   renderContentHeader() {
     const data = this.props.data;
-    
+
     return (
       <div>
-        <h1 className="title display1">{data.title}</h1>
+        <h1 className={`title display1`}>{data.title}</h1>
         <p className="subhead display4">{data.subhead}</p>
       </div>
     )
