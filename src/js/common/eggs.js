52f5f6836151874704a0616a6f6ba386559b0bcb
Madeleine <madeleine.auvinen@stinkstudios.com>
2018-01-05 14:02:53 -0500

    remove wip easter egg from develop

--- src/js/common/eggs.js
+++
@@ -1,5 +0,0 @@
-export const EGGS = [
-  'pizza',
-  'pasta',
-  'hamburger'
-]
--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -4,11 +4,9 @@ import Button from './Button.jsx'
 import RecipeContainer from './RecipeContainer.jsx'
 import Footer from './Footer.jsx'
 import PopupAbout from './PopupAbout.jsx'
-import Egg from './Egg.jsx'
 import Loader from './Loader.jsx'
 import styles from '../../scss/components/_app.scss'
 import { END_POINT } from "../common/end-points"
-import { EGGS } from "../common/eggs"
 import { BUTTON_LABELS } from "../common/button-labels"
 
 export default class App extends React.Component {
@@ -20,10 +18,7 @@ export default class App extends React.Component {
       dataIsLoaded: false,
       appData: null,
       recipeData: null,
-      popupIsOpen: false,
-      buttonClick: 0,
-      eggIsOpen: false,
-      currentEgg: null
+      popupIsOpen: false
     }
   }
 
@@ -79,38 +74,8 @@ export default class App extends React.Component {
     const currentRecipe = recipes[randomIndex];
 
     return (
-      this.setState({currentRecipe}),
-      this.setState({buttonClick: this.state.buttonClick + 1})
+      this.setState({currentRecipe})
     )
-  }
-
-  countButtonClick() {
-    if (this.state.buttonClick === 10) {
-      this.openEgg();
-    }
-  }
-
-  getCurrentEgg() {
-    const eggs = EGGS;
-    const randomIndex = [Math.floor(Math.random() * eggs.length)];
-    const currentEgg = eggs[randomIndex];
-    this.setState({currentEgg})
-  }
-
-  openEgg() {
-    this.getCurrentEgg();
-    setTimeout(() => {
-      this.setState({eggIsOpen: true})
-    }, 100)
-    
-    setTimeout(() => {
-      this.closeEgg();
-      this.setState({buttonClick: 0})
-    }, 800)
-  }
-
-  closeEgg() {
-    this.setState({eggIsOpen: false})
   }
 
   handlePopup = () => {
@@ -123,7 +88,6 @@ export default class App extends React.Component {
 
   handleClick = () => {
     this.getRecipe()
-    // this.countButtonClick()
   }
 
   render() {
@@ -140,7 +104,6 @@ export default class App extends React.Component {
           popupIsOpen={this.state.popupIsOpen} />
         <PopupAbout popupIsOpen={this.state.popupIsOpen} 
           data={this.state.appData} />
-        <Egg currentEgg={this.state.currentEgg} eggIsOpen={this.state.eggIsOpen} />
     
         { !this.state.dataIsLoaded ? <Loader /> : null }
       </div>
