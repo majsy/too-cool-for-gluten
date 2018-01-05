063e6f65328e0704a11550085849ebf110bc6b5e
Madeleine <madeleine.auvinen@stinkstudios.com>
2018-01-05 15:14:14 -0500

    add new thumbnail update meta description

--- dist/js/index.bundle.js
+++ dist/js/index.bundle.js
@@ -18819,21 +18819,15 @@ var _PopupAbout = __webpack_require__(44);
 
 var _PopupAbout2 = _interopRequireDefault(_PopupAbout);
 
-var _Egg = __webpack_require__(46);
-
-var _Egg2 = _interopRequireDefault(_Egg);
-
-var _Loader = __webpack_require__(48);
+var _Loader = __webpack_require__(46);
 
 var _Loader2 = _interopRequireDefault(_Loader);
 
-var _app = __webpack_require__(50);
+var _app = __webpack_require__(48);
 
 var _app2 = _interopRequireDefault(_app);
 
-var _endPoints = __webpack_require__(51);
-
-var _eggs = __webpack_require__(52);
+var _endPoints = __webpack_require__(49);
 
 var _buttonLabels = __webpack_require__(14);
 
@@ -18874,10 +18868,7 @@ var App = function (_React$Component) {
       dataIsLoaded: false,
       appData: null,
       recipeData: null,
-      popupIsOpen: false,
-      buttonClick: 0,
-      eggIsOpen: false,
-      currentEgg: null
+      popupIsOpen: false
     };
     return _this;
   }
@@ -18957,42 +18948,7 @@ var App = function (_React$Component) {
       var randomIndex = Math.floor(Math.random() * recipes.length);
       var currentRecipe = recipes[randomIndex];
 
-      return this.setState({ currentRecipe: currentRecipe }), this.setState({ buttonClick: this.state.buttonClick + 1 });
-    }
-  }, {
-    key: 'countButtonClick',
-    value: function countButtonClick() {
-      if (this.state.buttonClick === 10) {
-        this.openEgg();
-      }
-    }
-  }, {
-    key: 'getCurrentEgg',
-    value: function getCurrentEgg() {
-      var eggs = _eggs.EGGS;
-      var randomIndex = [Math.floor(Math.random() * eggs.length)];
-      var currentEgg = eggs[randomIndex];
-      this.setState({ currentEgg: currentEgg });
-    }
-  }, {
-    key: 'openEgg',
-    value: function openEgg() {
-      var _this5 = this;
-
-      this.getCurrentEgg();
-      setTimeout(function () {
-        _this5.setState({ eggIsOpen: true });
-      }, 100);
-
-      setTimeout(function () {
-        _this5.closeEgg();
-        _this5.setState({ buttonClick: 0 });
-      }, 800);
-    }
-  }, {
-    key: 'closeEgg',
-    value: function closeEgg() {
-      this.setState({ eggIsOpen: false });
+      return this.setState({ currentRecipe: currentRecipe });
     }
   }, {
     key: '__handlePopup__REACT_HOT_LOADER__',
@@ -19007,7 +18963,6 @@ var App = function (_React$Component) {
     key: '__handleClick__REACT_HOT_LOADER__',
     value: function __handleClick__REACT_HOT_LOADER__() {
       this.getRecipe();
-      // this.countButtonClick()
     }
   }, {
     key: 'render',
@@ -19026,7 +18981,6 @@ var App = function (_React$Component) {
           popupIsOpen: this.state.popupIsOpen }),
         _react2.default.createElement(_PopupAbout2.default, { popupIsOpen: this.state.popupIsOpen,
           data: this.state.appData }),
-        _react2.default.createElement(_Egg2.default, { currentEgg: this.state.currentEgg, eggIsOpen: this.state.eggIsOpen }),
         !this.state.dataIsLoaded ? _react2.default.createElement(_Loader2.default, null) : null
       );
     }
@@ -19743,95 +19697,7 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _egg = __webpack_require__(47);
-
-var _egg2 = _interopRequireDefault(_egg);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Egg = function (_React$Component) {
-  _inherits(Egg, _React$Component);
-
-  function Egg() {
-    _classCallCheck(this, Egg);
-
-    return _possibleConstructorReturn(this, (Egg.__proto__ || Object.getPrototypeOf(Egg)).apply(this, arguments));
-  }
-
-  _createClass(Egg, [{
-    key: 'renderContentEgg',
-    value: function renderContentEgg() {
-      var eggIsOpen = this.props.eggIsOpen;
-      var currentEgg = this.props.currentEgg;
-
-      return _react2.default.createElement(
-        'div',
-        { className: 'egg hamburger ' + (eggIsOpen ? 'isOpen' : '') },
-        _react2.default.createElement('img', { className: 'image', src: './static/images/hamburger.png' })
-      );
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var currentEgg = this.props.currentEgg;
-
-      return _react2.default.createElement(
-        'div',
-        null,
-        currentEgg ? this.renderContentEgg() : null
-      );
-    }
-  }]);
-
-  return Egg;
-}(_react2.default.Component);
-
-var _default = Egg;
-exports.default = _default;
-;
-
-var _temp = function () {
-  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-    return;
-  }
-
-  __REACT_HOT_LOADER__.register(Egg, 'Egg', '/Users/madeleineauvinen/Documents/projects/2cool4gluten/src/js/components/Egg.jsx');
-
-  __REACT_HOT_LOADER__.register(_default, 'default', '/Users/madeleineauvinen/Documents/projects/2cool4gluten/src/js/components/Egg.jsx');
-}();
-
-;
-
-/***/ }),
-/* 47 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 48 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__(0);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _loader = __webpack_require__(49);
+var _loader = __webpack_require__(47);
 
 var _loader2 = _interopRequireDefault(_loader);
 
@@ -19887,19 +19753,19 @@ var _temp = function () {
 ;
 
 /***/ }),
+/* 47 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 48 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
 /* 49 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 50 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 51 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19920,29 +19786,6 @@ var _temp = function () {
   }
 
   __REACT_HOT_LOADER__.register(END_POINT, 'END_POINT', '/Users/madeleineauvinen/Documents/projects/2cool4gluten/src/js/common/end-points.js');
-}();
-
-;
-
-/***/ }),
-/* 52 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var EGGS = exports.EGGS = ['pizza', 'pasta', 'hamburger'];
-;
-
-var _temp = function () {
-  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-    return;
-  }
-
-  __REACT_HOT_LOADER__.register(EGGS, 'EGGS', '/Users/madeleineauvinen/Documents/projects/2cool4gluten/src/js/common/eggs.js');
 }();
 
 ;
